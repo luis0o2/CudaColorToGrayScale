89a2e87f359a65623e84ccd0b624ce31768f2140
Luis Ochoa <ochoaluis0002@gmail.com>
2024-05-17 16:48:27 -0500

    Add project files.

---
+++ CudaColorToGrayScale/kernel.cu
@@ -0,0 +1,27 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <iostream>
+
+
+__global__ void ColorToGrayScaleConversion(unsigned char *Pout, unsigned char *Pin, int width, int height) {
+	int col = threadIdx.x + blockDim.x * blockIdx.x;
+	int row = threadIdx.y + blockDim.y * blockIdx.y;	
+
+	if (col < width && row < height) {
+		int grayOffset = row * width + col;		
+
+		int rgbOffset = grayOffset * CHANNELS;
+		unsigned char r = Pin[rgbOffset]; //RED VALUE
+		unsigned char g = Pin[rgbOffset + 1]; //GREEN VALUE
+		unsigned char b = Pin[rgbOffset + 2]; //BLUE VALUE
+
+		Pout[grayOffset] = 0.21f * r + 0.71f * g + 0.07f * b;
+	}
+
+}
+
+int main() {
+
+}
